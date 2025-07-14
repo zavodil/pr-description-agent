e072b0cdf40c53970fc524106e64ff3c2b4ec660
Vadim <support@zavodil.ru>
2025-07-14 21:04:32 +0300

    fix: port

--- app.py
+++ app.py
@@ -179,5 +179,5 @@ async def health():
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8088))
     uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
